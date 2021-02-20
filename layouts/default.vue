e165c98d2ce6f10141bea4482f1abf408c518bc9
A694392 <martyna.strzempek@atos.net>
2021-02-20 11:36:28 +0100

    First commit

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-footer>
+      <span class="footer text-center">Bubdzie company &copy; 2021</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<style lang="scss">
+.footer {
+  width: 100%;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      Hello World
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
